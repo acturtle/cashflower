1bf8d56b9410a46fd17ab68021f852a30914fe98
zchmielewska <zchmielewska@gmail.com>
2022-07-31 19:17:33 +0200

    enforce using ModelVariable in policy.py

--- cashflower/model.py
+++ cashflower/model.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import inspect
 
-from functools import lru_cache
+from .utils import list_used_words, unique_extend
 
 
 class ModelPoint:
@@ -19,49 +20,85 @@ class ModelPoint:
 
 
 class ModelVariable:
-    def __init__(self, function, settings):
-        self.function = function
-        self.settings = settings
-        self.name = function.__name__
-        self.t_max = settings["T_MAX"] + 1
-        self.result = [None] *self.t_max
+    instances = []
+
+    def __init__(self, name):
+        self.__class__.instances.append(self)
+        self.name = name
+        self.formula = None
+        self.result = [None] * 1440
+        self.children = []
+        self.grandchildren = []
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
 
     def __call__(self, t):
-        if t < 0 or t >= self.t_max:
+        if t < 0 or t >= 1440:
             return 0
-        if not self.result[t]:
+        elif self.result[t] is None:
             self.result[t] = self.formula(t)
         return self.result[t]
 
-    @lru_cache
-    def formula(self, t):
-        return self.function(t)
-
-    def clear(self):
-        self.result = [None] * self.t_max
+    def __lt__(self, other):
+        return len(self.grandchildren) < len(other.grandchildren)
 
     def calculate(self):
         try:
-            self.result = [self(t) for t in range(self.t_max)]
+            for t in range(1440):
+                self.result[t] = self(t)
         except RecursionError:
-            for t in range(self.t_max - 1, -1, -1):
+            for t in range(1440-1, -1, -1):
                 self.result[t] = self(t)
 
 
 class Model:
-    def __init__(self, model_point, model_variables):
+    def __init__(self, model_point, variables):
         self.model_point = model_point
-        self.model_variables = model_variables
+        self.variables = variables
         self.output = None
 
+    def get_variable(self, name):
+        for variable in self.variables:
+            if variable.name == name:
+                return variable
+
+    def set_children(self):
+        variable_names = [variable.name for variable in self.variables]
+        for variable in self.variables:
+            formula_source = inspect.getsource(variable.formula)
+            child_names = list_used_words(formula_source, variable_names)  # TODO too primitive and error-prone
+            for child_name in child_names:
+                if child_name != variable.name:
+                    child = self.get_variable(child_name)
+                    variable.children.append(child)
+
+    def set_grandchildren(self):
+        for variable in self.variables:
+            variable.grandchildren = list(variable.children)
+            for grandchild in variable.grandchildren:
+                variable.grandchildren = unique_extend(variable.grandchildren, grandchild.children)
+
+    def sort(self):
+        self.variables = sorted(self.variables)
+
+    def remove_from_grandchildren(self, removed_variable):
+        for variable in self.variables:
+            if removed_variable in variable.grandchildren:
+                variable.grandchildren.remove(removed_variable)
+
     def calculate(self):
+        self.sort()
+        for variable in self.variables:
+            variable.calculate()
+            self.remove_from_grandchildren(variable)
+            self.sort()
+
+    def set_output(self):
         output = pd.DataFrame()
-        for i in range(len(self.model_point)):
-            policy_output = pd.DataFrame()
-            for model_variable in self.model_variables:
-                model_variable.clear()
-                model_variable.calculate()
-                policy_output[model_variable.name] = model_variable.result
-            self.model_point.record_num += 1
-            output = pd.concat([output, policy_output])
+        for variable in self.variables:
+            output[variable.name] = variable.result
         self.output = output
