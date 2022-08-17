a2d4a51e740f6e16b09aa26a234ca3e40f197cc5
zchmielewska <zchmielewska@gmail.com>
2022-08-17 17:41:47 +0200

    introduce settings to the model

--- cashflower/__init__.py
+++ cashflower/__init__.py
@@ -1,5 +1,3 @@
-import time
-
 import datetime
 import functools
 import inspect
@@ -18,22 +16,61 @@ def assign(var):
     return wrapper
 
 
-def get_model_input(modelpoint_module, model_module):
+def get_model_input(modelpoint_module, model_module, settings):
+    """Prepares input for model.
+
+    Gathers all model points and model variable instances.
+    Assigns names to model variables and, if not assigned by user, also model point.
+    (Thanks to that user doesn't have to assign model point to model variable if there is only one model point)
+
+    Parameters
+    ----------
+    modelpoint_module : module where user defines model points
+    model_module : module where user defines model variables
+    settings : dictionary with user settings
+
+    Returns
+    -------
+    tuple containing two lists (variables and model points)
+    """
+
+    # Gather model points
     modelpoint_members = inspect.getmembers(modelpoint_module)
     modelpoints = [m[1] for m in modelpoint_members if isinstance(m[1], ModelPoint)]
     first_modelpoint = modelpoints[0]
 
+    for modelpoint in modelpoints:
+        modelpoint.settings = settings
+
+    # Gather model variables
     model_members = inspect.getmembers(model_module)
     model_members = [m for m in model_members if isinstance(m[1], ModelVariable)]
 
     variables = []
     for name, variable in model_members:
         variable.name = name
+        variable.settings = settings
         if variable.modelpoint is None:
             variable.modelpoint = first_modelpoint
         variables.append(variable)
 
     return variables, modelpoints
+
+
+def load_settings(settings):
+    initial_settings = {
+        "POLICY_ID_COLUMN": "POLICY_ID",
+        "T_CALCULATION_MAX": 1440,
+        "T_OUTPUT_MAX": 1440,
+        "AGGREGATE": False,
+        "OUTPUT_COLUMNS": [],
+    }
+
+    for key, value in settings.items():
+        initial_settings[key] = value
+
+    return initial_settings
+
 
 
 class ModelPoint:
@@ -42,7 +79,7 @@ class ModelPoint:
     data = data frame with data for all policies
     policy_id = id for the current policy that is being calculated
     record_num = if the given policy has multiple records, which record is currently used
-    size = how many records the curreny policy have (>=1)
+    size = how many records does the current policy have
     policy_data = data frame with rows only for the current policy_id
     policy_record = row of data frame for the current policy_id and record_num
     """
@@ -58,6 +95,7 @@ class ModelPoint:
         self.size = 0
         self.policy_data = None
         self.policy_record = None
+        self.settings = None
 
     def __repr__(self):
         return f"MP: {self.name}"
@@ -76,8 +114,9 @@ class ModelPoint:
     @policy_id.setter
     def policy_id(self, new_policy_id):
         """ Policy id in model point is changed by model.calculated_all_policies() """
+        policy_id_column = self.settings["POLICY_ID_COLUMN"]
         self._policy_id = new_policy_id
-        self.policy_data = self.data[self.data["POLICY_ID"] == new_policy_id]
+        self.policy_data = self.data[self.data[policy_id_column] == new_policy_id]
         self.size = self.policy_data.shape[0]
 
     @property
@@ -104,6 +143,7 @@ class ModelVariable:
     children = only relevant in model context (because then we know the groups of variables),
         which variables is this function calling?
     grandchildren = children and their descendants (i.e. also grandgrandchildren and so on)
+    settings = user settings
     """
     instances = []
 
@@ -116,6 +156,7 @@ class ModelVariable:
         self.result = None
         self.children = []
         self.grandchildren = []
+        self.settings = None
 
     def __repr__(self):
         return f"MV: {self.name}"
@@ -124,17 +165,19 @@ class ModelVariable:
         return len(self.grandchildren) < len(other.grandchildren)
 
     def __call__(self, t, r=None):
-        if t < 0 or t >= 1440:
+        t_calculation_max = self.settings["T_CALCULATION_MAX"]
+        if t < 0 or t > t_calculation_max:
             return 0
 
-        # User calls lower order variable from higher order variable
+        # User might call lower order variable from higher order variable and specify record (r)
         if r is not None:
             return self.result[r][t]
 
         return self.formula(t)
 
     def calculate(self):
-        self.result = [[None] * 1440 for _ in range(self.modelpoint.size)]
+        t_calculation_max = self.settings["T_CALCULATION_MAX"]
+        self.result = [[None] * t_calculation_max for _ in range(self.modelpoint.size)]
 
         for r in range(self.modelpoint.size):
             self.modelpoint.record_num = r
@@ -142,9 +185,11 @@ class ModelVariable:
 
             # The try-except formula helps with autorecursive functions
             try:
-                self.result[r] = list(map(self.formula, range(1440)))
+                self.result[r] = list(map(self.formula, range(t_calculation_max+1)))
             except RecursionError:
-                self.result[r] = list(map(self.formula, range(1440-1, -1, -1)))
+                lst = list(map(self.formula, range(t_calculation_max, -1, -1)))
+                lst.reverse()
+                self.result[r] = lst
 
     def clear(self):
         if self.recalc:
@@ -158,13 +203,16 @@ class Model:
 
      variables = list of model variable objects
      modelpoints = list of model point objects
+     settings = user's settings
      queue = in which order should variables be calculated?
+     empty_output = empty dict with keys prepared for output
      output = dict with key=modelpoints, values=data frames with column names are variable names
 
      """
-    def __init__(self, variables, modelpoints):
+    def __init__(self, variables, modelpoints, settings):
         self.variables = variables
         self.modelpoints = modelpoints
+        self.settings = settings
         self.queue = []
         self.empty_output = None
         self.output = None
@@ -221,9 +269,13 @@ class Model:
         self.empty_output = empty_output
 
     def get_empty_output(self):
+        aggregate = self.settings["AGGREGATE"]
         empty_output = dict()
         for modelpoint in self.modelpoints:
             empty_output[modelpoint.name] = pd.DataFrame()
+            empty_output[modelpoint.name]["t"] = None
+            if not aggregate:
+                empty_output[modelpoint.name]["r"] = None
 
         for variable in self.variables:
             empty_output[variable.modelpoint.name][variable.name] = None
@@ -234,39 +286,53 @@ class Model:
             variable.clear()
 
     def calculate_one_policy(self):
+        aggregate = self.settings["AGGREGATE"]
+        t_output_max = min(self.settings["T_OUTPUT_MAX"], self.settings["T_CALCULATION_MAX"])
         output = self.get_empty_output()
+
+        # output contains time and record number
+        if not aggregate:
+            for modelpoint in self.modelpoints:
+                output[modelpoint.name]["t"] = list(range(t_output_max+1)) * modelpoint.size
+                output[modelpoint.name]["r"] = utils.repeated_numbers(modelpoint.size, t_output_max+1)
 
         # variable.result is a list of lists
         for variable in self.queue:
             variable.calculate()
-            output[variable.modelpoint.name][variable.name] = utils.flatten(variable.result)
-
-        # output contains time and record number
-        for modelpoint in self.modelpoints:
-            output[modelpoint.name]["t"] = list(range(1440)) * modelpoint.size
-            output[modelpoint.name]["r"] = utils.repeated_numbers(modelpoint.size, 1440)
+            if aggregate:
+                output[variable.modelpoint.name][variable.name] = utils.aggregate(variable.result, n=t_output_max+1)
+            else:
+                output[variable.modelpoint.name][variable.name] = utils.flatten(variable.result, n=t_output_max+1)
 
         return output
 
     def calculate_all_policies(self):
+        aggregate = self.settings["AGGREGATE"]
+        t_output_max = min(self.settings["T_OUTPUT_MAX"], self.settings["T_CALCULATION_MAX"])
         output = self.get_empty_output()
         primary = self.get_modelpoint("policy")
 
+        policy_outputs = []
         for row in range(len(primary)):
-            start = time.time()
-
             # Primary modelpoint has unique column with policy ID
-            policy_id = primary.data.iloc[row]["POLICY_ID"]
+            policy_id_column = self.settings["POLICY_ID_COLUMN"]
+            policy_id = primary.data.iloc[row][policy_id_column]
 
             # All modelpoints must have the same policy ID
             for modelpoint in self.modelpoints:
                 modelpoint.policy_id = policy_id
 
             self.clear_variables()
-            policy_output = self.calculate_one_policy()
 
-            for modelpoint in self.modelpoints:
-                output[modelpoint.name] = pd.concat([output[modelpoint.name], policy_output[modelpoint.name]])
+            policy_output = self.calculate_one_policy()
+            policy_outputs.append(policy_output)
+
+        for modelpoint in self.modelpoints:
+            if aggregate:
+                output[modelpoint.name] = sum(policy_output[modelpoint.name] for policy_output in policy_outputs)
+                output[modelpoint.name]["t"] = list(range(t_output_max+1))
+            else:
+                output[modelpoint.name] = pd.concat(policy_output[modelpoint.name] for policy_output in policy_outputs)
 
         self.output = output
 
@@ -282,7 +348,17 @@ class Model:
         if not os.path.exists("output"):
             os.makedirs("output")
 
-        for mp in ModelPoint.instances:
+        output_columns = self.settings["OUTPUT_COLUMNS"]
+        user_chose_columns = len(output_columns) > 0
+
+        for mp in self.modelpoints:
+            output = self.output.get(mp.name)
+            if user_chose_columns:
+                output_columns.extend(["t", "r"])
+                columns = output.columns.intersection(output_columns)
+            else:
+                columns = output.columns
+
             filepath = f"output/{timestamp}_{mp.name}.csv"
-            self.output.get(mp.name).to_csv(filepath)
+            output.to_csv(filepath, index=False, columns=columns)
 
