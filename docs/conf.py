ec1cc9529796b7d965ee2dbaa2094eb866201cbb
zchmielewska <zchmielewska@gmail.com>
2022-11-16 18:42:17 +0100

    change theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'cashflower'
-copyright = '2022, Zuzanna Chmielewska'
-author = 'Zuzanna Chmielewska'
+copyright = '2022, Zuzanna'
+author = 'Zuzanna'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
