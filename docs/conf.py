6106d20967c4b04b32ba52c321cc6981c56505f5
zchmielewska <zchmielewska@gmail.com>
2022-11-17 19:28:12 +0100

    test furo

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Zuzanna'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['furo']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'furo_theme'
 html_static_path = ['_static']
