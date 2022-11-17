5d5efa71a03d605b4ebe1a8d448c4f5f307eee30
zchmielewska <zchmielewska@gmail.com>
2022-11-17 19:39:03 +0100

    test

--- docs/conf.py
+++ docs/conf.py
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
