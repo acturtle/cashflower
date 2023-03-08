588541495213f0d71fbfa52643e634400f54f90b
zchmielewska <zchmielewska@gmail.com>
2023-03-08 21:11:57 +0100

    update tutorials

--- tutorials/annuity/deferred/run.py
+++ tutorials/annuity/deferred/run.py
@@ -1,8 +1,18 @@
 import sys
-
-from cashflower import start
+from functools import partial
+from multiprocessing import cpu_count, Pool
+from cashflower import execute, merge_and_save, start
 from tutorials.annuity.deferred.settings import settings
 
 
 if __name__ == "__main__":
-    start("tutorials.annuity.deferred", settings, sys.argv)
+    model_name = "tutorials.annuity.deferred"
+
+    if settings.get("MULTIPROCESSING"):
+        cpu_count = cpu_count()
+        p = partial(execute, model_name=model_name, settings=settings, cpu_count=cpu_count, argv=sys.argv)
+        with Pool(cpu_count) as pool:
+            outputs = pool.map(p, range(cpu_count))
+        merge_and_save(outputs, settings)
+    else:
+        start(model_name, settings, sys.argv)
