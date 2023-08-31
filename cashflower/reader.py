2ed13fa2f7cfc5bb058c7e738cebfd334fc3c017
zchmielewska <zchmielewska@gmail.com>
2023-08-31 14:57:06 +0200

    update reader class

--- cashflower/reader.py
+++ cashflower/reader.py
@@ -3,22 +3,26 @@ from collections import defaultdict
 
 
 class CSVReader:
-    def __init__(self, filename):
+    def __init__(self, filename, num_row_label_cols=1):
         self.filename = filename
+        self.n = num_row_label_cols
         self.data = defaultdict(dict)
         self.row_index = None
         self.col_index = None
-        self.load_data()
+        if self.n == 1:
+            self.load_data_n_equal_1()
+        else:
+            self.load_data_n_greater_than_1()
 
-    def load_data(self):
+    def load_data_n_equal_1(self):
         with open(self.filename, 'r') as file:
             reader = csv.reader(file)
             header = next(reader)
             self.row_index = {row[0]: i for i, row in enumerate(reader)}
             self.col_index = {label: i for i, label in enumerate(header[1:])}
 
-            file.seek(0)  # Reset the file pointer to the beginning
-            next(reader)  # Skip the header line
+            file.seek(0)
+            next(reader)
 
             for row in reader:
                 row_label = row[0]
@@ -27,9 +31,28 @@ class CSVReader:
                         if col_label in self.col_index:
                             self.data[row_label][col_label] = cell_value
 
+    def load_data_n_greater_than_1(self):
+        with open(self.filename, 'r') as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            self.row_index = {tuple(row[:self.n]): i for i, row in enumerate(reader)}
+            self.col_index = {label: i for i, label in enumerate(header[self.n:])}
+
+            file.seek(0)
+            next(reader)
+
+            for row in reader:
+                row_label = tuple(row[:self.n])
+                if row_label in self.row_index:
+                    for col_label, cell_value in zip(header[self.n:], row[self.n:]):
+                        if col_label in self.col_index:
+                            self.data[row_label][col_label] = cell_value
+
     def get_value(self, row_label, col_label):
         value = self.data.get(row_label, {}).get(col_label, None)
         if value is not None:
             return value
         else:
-            raise ValueError("Row or column label not found.")
+            raise ValueError(f"Row '{row_label}' or column '{col_label}' label not found in '{self.filename}'.\n"
+                             f"Please ensure that row label(s) and column label are strings.")
+
