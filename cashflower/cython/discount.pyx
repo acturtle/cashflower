882030fb2b0487217aac1fe279ffc6a655c9e781
zchmielewska <zchmielewska@gmail.com>
2023-09-28 17:14:35 +0200

    add decorators to discount function

--- cashflower/cython/discount.pyx
+++ cashflower/cython/discount.pyx
@@ -1,15 +1,23 @@
+cimport cython
 cimport numpy as np
 import numpy as np
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cpdef np.ndarray[double] discount(np.ndarray[double] cash_flows, np.ndarray[double] discount_rates):
-    cdef int n = cash_flows.shape[0]
-    cdef np.ndarray[double] result = np.empty(n)
     cdef int t
+    cdef int n1 = cash_flows.shape[0]
+    cdef int n2 = discount_rates.shape[0]
+    cdef np.ndarray[double] result = np.empty(n1)
 
-    result[-1] = cash_flows[-1]
+    # The length of both array must be the same
+    if n1 != n2:
+        raise ValueError("Arrays must have the same length.")
 
-    for t in range(n-2, -1, -1):
+    result[n1-1] = cash_flows[n1-1]
+
+    for t in range(n1-2, -1, -1):
         result[t] = cash_flows[t] + result[t+1] * discount_rates[t+1]
 
     return result
