546e5b40b8f8e53c06932a79572f5bd8a2c0ae43
zchmielewska <zchmielewska@gmail.com>
2024-05-08 08:59:27 +0200

    simplify

--- dev_models/dev_model_22/model.py
+++ dev_models/dev_model_22/model.py
@@ -1,48 +1,14 @@
 from cashflower import variable
 
+
 @variable()
-def NoPolsBoP(t):
-    """No of policies at the start of period"""
-    if ((t==0 or t>288)) :
-        return 0
-    elif (t==1) :
+def a(t):
+    return b(t-1)
+
+
+@variable()
+def b(t):
+    if t == 0:
         return 1
     else:
-        return NoPolsEoP(t-1)-MattNo(t-1)
-
-@variable()
-def MatRateM(t):
-    return 0.01
-
-@variable()
-def LapseRateD(t):
-    return 0.01
-
-@variable()
-def DeathRateD(t):
-    return 0.01
-
-@variable()
-def SurrNo(t):
-    return NoPolsBoP(t)*LapseRateD(t)
-
-@variable()
-def DeathNo(t):
-    return NoPolsBoP(t)*DeathRateD(t)
-
-
-@variable()
-def MattNo(t):
-    if (t==0) :
-        return 0
-    else:
-        return MatRateM(t)*NoPolsEoP(t-1)
-
-@variable()
-def NoPolsEoP(t):
-    if (t==0) :
-        return 1
-    elif (t>288) :
-        return 0
-    else:
-        return NoPolsBoP(t)-SurrNo(t)-DeathNo(t)
+        return a(t)
