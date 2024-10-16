23988cb568b3c019ada0e8a9477fa17db454654c
zchmielewska <zchmielewska@gmail.com>
2024-10-16 14:29:26 +0200

    finalize change

--- dev_models/dev_model_07/settings.py
+++ dev_models/dev_model_07/settings.py
@@ -5,7 +5,7 @@ settings = {
     "NUM_STOCHASTIC_SCENARIOS": None,
     "OUTPUT_COLUMNS": ["f"],
     "SAVE_DIAGNOSTIC": True,
-    "SAVE_LOG": True,
+    "SAVE_LOG": False,
     "SAVE_OUTPUT": True,
     "T_MAX_CALCULATION": 720,
     "T_MAX_OUTPUT": 720,
--- dev_models/dev_model_11/settings.py
+++ dev_models/dev_model_11/settings.py
@@ -5,7 +5,7 @@ settings = {
     "NUM_STOCHASTIC_SCENARIOS": None,
     "OUTPUT_COLUMNS": [],
     "SAVE_DIAGNOSTIC": True,
-    "SAVE_LOG": True,
+    "SAVE_LOG": False,
     "SAVE_OUTPUT": True,
     "T_MAX_CALCULATION": 360,
     "T_MAX_OUTPUT": 360,
