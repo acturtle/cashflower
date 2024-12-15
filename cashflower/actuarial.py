6bb07e922f01e377c80eb3a603b2b25ca7d3bb5a
zchmielewska <zchmielewska@gmail.com>
2024-12-15 15:06:52 +0100

    rmv unnecessary brackets

--- cashflower/actuarial.py
+++ cashflower/actuarial.py
@@ -2,6 +2,6 @@ def annuity_certain(i, n, timing="immediate"):
     if timing == "immediate":
         return (1 - (1 + i) ** (-n) / i) * (1 + i)
     elif timing == "due":
-        return (1 - (1 + i) ** (-n) / i)
+        return 1 - (1 + i) ** (-n) / i
     else:
         raise ValueError("Invalid timing. Must be 'due' or 'immediate'.")
