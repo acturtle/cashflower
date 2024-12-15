1a2828577830a9f58360f0408a8c8285e7c03685
zchmielewska <zchmielewska@gmail.com>
2024-12-15 15:06:26 +0100

    add actuarial module

---
+++ cashflower/actuarial.py
@@ -0,0 +1,7 @@
+def annuity_certain(i, n, timing="immediate"):
+    if timing == "immediate":
+        return (1 - (1 + i) ** (-n) / i) * (1 + i)
+    elif timing == "due":
+        return (1 - (1 + i) ** (-n) / i)
+    else:
+        raise ValueError("Invalid timing. Must be 'due' or 'immediate'.")
