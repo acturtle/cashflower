8495bc7f4fa1bb8cbce68fc6f7395d537549bf75
zchmielewska <zchmielewska@gmail.com>
2025-01-11 15:43:38 +0100

    add duration

--- cashflower/actuarial.py
+++ cashflower/actuarial.py
@@ -5,3 +5,6 @@ def annuity_certain(i, n, timing="immediate"):
         return 1 - (1 + i) ** (-n) / i
     else:
         raise ValueError("Invalid timing. Must be 'due' or 'immediate'.")
+
+
+# Macaulay duration
