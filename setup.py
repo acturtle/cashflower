85d839375d55da5500c85855a3fd66a3fe892ae8
zchmielewska <zchmielewska@gmail.com>
2025-04-30 20:43:40 +0200

    increment version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extensions = cythonize(
 
 setup(
     name="cashflower",
-    version="0.10.2",
+    version="0.10.3",
 
     author="Zuzanna Chmielewska",
     description="Framework for actuarial cash flow models",
