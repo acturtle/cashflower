575420a01bda58473ecb6c520163982d8d63f6e9
zchmielewska <zchmielewska@gmail.com>
2025-07-12 12:57:20 +0200

    rmv dev_model_27

--- tests/dev_models/dev_model_27/input.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from cashflower import ModelPointSet
-
-my_data = pd.read_csv("data.csv")
-
-policy = ModelPointSet(data=pd.DataFrame({
-    "id": range(10_000)
-}))
--- tests/dev_models/dev_model_27/run.py
+++
@@ -1,8 +0,0 @@
-import os
-from cashflower import run
-from settings import settings  # Import settings from the settings.py file
-
-
-# Main entry point for running the model
-if __name__ == "__main__":
-    output, diagnostic, log = run(settings=settings, path=os.path.dirname(__file__))
